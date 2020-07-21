0948cf71c400f489c6609235b508f63123bc5318
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-21 20:26:05 +0000

    Add transformYaml function to preserve file format
    This change enables injecting new release branch.
    
    Change-Id: I931e96d4a61cc972968e8ec46b9bd0379b3fb592

--- test/RpmSpec.hs
+++ test/RpmSpec.hs
@@ -1,9 +1,13 @@
 module Main (main) where
 
+import qualified Data.ByteString.Lazy as BSL
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import qualified Data.Text.IO as T
+import Sfinfo.Data
 import Sfinfo.RpmSpec
+import Sfinfo.Yaml
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -11,10 +15,38 @@ main :: IO ()
 main = do
   beforeSpec <- T.readFile "./test/data/python-APScheduler.spec.original"
   afterSpec <- T.readFile "./test/data/python-APScheduler.spec.updated"
-  defaultMain (tests (beforeSpec, afterSpec))
+  beforeYaml <- T.readFile "./test/data/resources.yaml.original"
+  afterYaml <- T.readFile "./test/data/resources.yaml.updated"
+  defaultMain (tests (beforeSpec, afterSpec) (beforeYaml, afterYaml))
 
-tests :: (Text, Text) -> TestTree
-tests dataFiles = testGroup "Tests" [unitTests dataFiles]
+tests :: (Text, Text) -> (Text, Text) -> TestTree
+tests dataFiles yamlFiles =
+  testGroup
+    "Tests"
+    [ unitTests dataFiles,
+      yamlTests yamlFiles
+    ]
+
+encode :: Text -> BSL.ByteString
+encode = BSL.fromStrict . T.encodeUtf8
+
+decode :: BSL.ByteString -> Text
+decode = T.decodeUtf8 . BSL.toStrict
+
+yamlTests :: (Text, Text) -> TestTree
+yamlTests (beforeYaml, afterYaml) =
+  testGroup
+    "yaml"
+    [ testCase "yamlfile updated" $
+        assertEqual
+          "Comment are preserved"
+          afterYaml
+          (decode $ transformYaml addReleaseBranch (encode beforeYaml))
+    ]
+  where
+    addReleaseBranch :: Maybe Text -> SFRepo -> SFRepo
+    addReleaseBranch (Just "rpms/zuul") repo = addBranch "3.5" "master" repo
+    addReleaseBranch _ repo = repo
 
 unitTests :: (Text, Text) -> TestTree
 unitTests (beforeSpec, afterSpec) =
