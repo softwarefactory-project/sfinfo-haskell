cfbda1f21b0ad4278bfb619770fdfdd02ff916ea
Tristan Cacqueray <tdecacqu@redhat.com>
2021-02-22 20:27:20 +0000

    Add approve-reviews helper
    This change adds a new cli command to approve reviews when zuul is
    quiet.
    
    Depends-On: https://softwarefactory-project.io/r/21161
    Change-Id: Ifa5429f2deff38dde2695fbd03adb7bd703914a2

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Gerrit
-import Sfinfo (comparePipAndRpm, proposeExecutorAnsibleUpdate, proposeUpdate)
+import Sfinfo (approveReview, comparePipAndRpm, proposeExecutorAnsibleUpdate, proposeUpdate)
 import qualified Sfinfo
 import Turtle (FilePath, Parser, argPath, argText, need, options, subcommand, (<|>))
 import Prelude hiding (FilePath)
@@ -12,6 +12,7 @@ data Command
   = PackageWithoutReview FilePath
   | GetReviewStatus FilePath
   | ComputeDiff FilePath
+  | ApproveReview FilePath Text
   | ProposeUpdate FilePath Text
   | ProposeExecutorUpdate FilePath Text
   deriving (Show)
@@ -22,6 +23,7 @@ usage =
   computeDiffUsage
     <|> proposeUpdateUsage
     <|> proposeExecutorUpdateUsage
+    <|> approveReviewUsage
     <|> getReviewStatusUsage
     <|> getPackageWithoutReview
   where
@@ -45,6 +47,14 @@ usage =
         "Generate git reviews to bump outdated packages"
         ( ProposeUpdate
             <$> argPath "outdated-list" "gen-diff output file name"
+            <*> argText "gerrit-user" "Gerrit ssh name user to push review (and set GERRIT_PASSWORD environment variable for approval)"
+        )
+    approveReviewUsage =
+      subcommand
+        "approve-reviews"
+        "Approve review when the gate is quiet"
+        ( ApproveReview
+            <$> argPath "review-list" "A list of review"
             <*> argText "gerrit-user" "Gerrit ssh name user to push review (and set GERRIT_PASSWORD environment variable for approval)"
         )
     computeDiffUsage =
@@ -73,6 +83,8 @@ main =
       GetReviewStatus sfinfoFile -> sfinfoProcess sfinfoFile Sfinfo.getReviewStatus
       ComputeDiff outputFile ->
         comparePipAndRpm outputFile
+      ApproveReview reviewList gerritUser ->
+        approveReview gerritUser reviewList
       ProposeUpdate outdatedList gerritUser ->
         proposeUpdate (fromMaybe "/home/fedora" home') gerritUser "sf-master" outdatedList
       ProposeExecutorUpdate sfinfoFile gerritUser ->
